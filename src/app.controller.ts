f1593142b46d22d9ae893334c5f076e02721b496
florinpana01 <florinpana98@gmail.com>
2023-11-15 14:35:19 +0100

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  
   @Get()
   getHello(): string {
     return this.appService.getHello();
